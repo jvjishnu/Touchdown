08add85173eddb21dad1d4420699842556da24e0
jvijayan <jishnu.vijayan.contractor@move.com>
2024-01-14 16:37:21 -0800

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by jvijayan on 2024-01-14.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
